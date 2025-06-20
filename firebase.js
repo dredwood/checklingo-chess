34d826979ed8cb2dee9f152b4aa65ed7dc76787a
dredwood <sausagegnx@gmail.com>
2025-06-20 18:00:08 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -11,15 +11,3 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
-
-// Create user XP entry if it doesn't exist
-auth.onAuthStateChanged(user => {
-  if (user) {
-    const userRef = db.collection("users").doc(user.uid);
-    userRef.get().then(doc => {
-      if (!doc.exists) {
-        userRef.set({ xp: 0 });
-      }
-    });
-  }
-});
